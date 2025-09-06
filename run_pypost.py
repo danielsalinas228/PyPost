dafd809648176af3ad4875752597a06a73a31634
Daniel Salinas <62160952+danielsalinas228@users.noreply.github.com>
2025-09-06 00:23:03 -0600

    Implements logic to allow user send pending letters

--- run_pypost.py
+++ run_pypost.py
@@ -2,23 +2,42 @@ import sys
 from pathlib import Path
 from modules.pypost import PyPost
 
+
 def main():
-	if len(sys.argv) != 2:
-		print("Usage: python3 run_pypost.py letter_name (without .txt)")
+	if len(sys.argv) < 2:
+		print("Usage:")
+		print("  python3 run_pypost.py --submit letter_name (without .txt)")
+		print("  python3 run_pypost.py --send_pending_letters")
 		sys.exit(1)
-	letter_path = "LettersToSend/" + sys.argv[1] + ".txt"
+
+	flag = sys.argv[1]
 	pypost = PyPost()
-	letter_id = pypost.submit_letter(letter_path)
-	if letter_id:
-		print(f"Letter submitted successfully! Letter ID: {letter_id}")
+
+	if flag == "--submit":
+		if len(sys.argv) != 3:
+			print("Usage: python3 run_pypost.py --submit letter_name (without .txt)")
+			sys.exit(1)
+		letter_name = sys.argv[2]
+		letter_path = f"LettersToSend/{letter_name}.txt"
+		letter_id = pypost.submit_letter(letter_path)
+		if letter_id:
+			print(f"Letter submitted successfully! Letter ID: {letter_id}")
+		else:
+			print("Failed to submit letter.")
+		# Delete the submitted letter file
+		try:
+			Path(letter_path).unlink(missing_ok=True)
+			print(f"Deleted file: {letter_path}")
+		except Exception as e:
+			print(f"Could not delete file {letter_path}: {e}")
+	elif flag == "--send_pending_letters":
+		sent_letters_ids = pypost.send_pending_letters()
+		if sent_letters_ids:
+			print(f"Letters sent successfully!: \n{', '.join(sent_letters_ids)}")
+		else:
+			print("No pending letters sent.")
 	else:
-		print("Failed to submit letter.")
-	# Delete the submitted letter file
-	try:
-		Path(letter_path).unlink(missing_ok=True)
-		print(f"Deleted file: {letter_path}")
-	except Exception as e:
-		print(f"Could not delete file {letter_path}: {e}")
+		print("Unknown flag. Use --submit or --send_pending_letters.")
 
 if __name__ == "__main__":
 	main()
